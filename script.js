90f655c5ecbaffb6011200193e6f1fdde7277556
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-16 08:28:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 function firstWord(s) {
   // your code here
+	if (s == null || s.length() == 0)
+	{
+		return "";
+	}
+
+	string word = "";
+
+	for (let i = 0; i < s.length; i++) {
+		if(s.charAt() != "")
+		{
+			word = word + s.charAt(i);
+		}else
+		{
+			break;
+		}
+	}
+
+	return word;
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
